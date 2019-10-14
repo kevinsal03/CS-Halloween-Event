44aec2adfa96946bb2e92a0e9639da053278e6d5
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-10-14 16:52:22 -0400

    Basic plugin layout

---
+++ src/main/java/me/kevsal/minecraft/cshalloween/Main.java
@@ -0,0 +1,18 @@
+package me.kevsal.minecraft.cshalloween;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @EventHandler
+    public void onEnable() {
+
+    }
+
+    @EventHandler
+    public void onDisable() {
+
+    }
+
+}
