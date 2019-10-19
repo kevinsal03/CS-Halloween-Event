526cf38be80fd1796b3664c894653af15efa696e
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-10-19 13:25:48 -0400

    Fix error thrown when there are no players online.

--- src/main/java/me/kevsal/minecraft/cshalloween/TaskSound.java
+++ src/main/java/me/kevsal/minecraft/cshalloween/TaskSound.java
@@ -22,16 +22,19 @@ public class TaskSound implements Runnable {
             //array of sounds
             Sound[] sounds = new Sound[]{Sound.ENTITY_BAT_DEATH, Sound.AMBIENT_CAVE, Sound.BLOCK_ANVIL_USE, Sound.BLOCK_DISPENSER_DISPENSE, Sound.ENTITY_DROWNED_DEATH_WATER, Sound.ENTITY_ILLUSIONER_CAST_SPELL, Sound.BLOCK_GLASS_BREAK, Sound.ENTITY_PLAYER_BURP, Sound.ENTITY_PLAYER_HURT}; //sounds
 
-            //select the player
-            Player p = playerArray[rand.nextInt(playerArray.length)];
-            //select the sound
-            Sound sound = sounds[rand.nextInt(sounds.length)];
+            if (playerArray.length != 0) {
+                //select the player
+                Player p = playerArray[rand.nextInt(playerArray.length)];
+                //select the sound
+                Sound sound = sounds[rand.nextInt(sounds.length)];
 
-            //play the sound
-            p.playSound(p.getLocation(), sound, 50, 0.5F);
+                //play the sound
+                p.playSound(p.getLocation(), sound, 50, 0.5F);
 
-            Bukkit.getServer().getLogger().info("Sound task ran and played a sound");
-
+                Bukkit.getServer().getLogger().info("Sound task ran and played a sound");
+            } else {
+                Bukkit.getServer().getLogger().info("No online players, no sound played.");
+            }
 
         } else {
             //no playsound
